7342867c16a28fad92ec14446f1d14c49124e88e
zhajikun <jikuinzha@gmail.com>
2021-06-30 21:56:02 -0500

    Initial commit

---
+++ src/test/java/com/bluenile/scrabble/ScrabbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluenile.scrabble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrabbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
